d8cb20afc92cdf3b065033e6ae3db74a968759e0
Emily <emily-l93@hotmail.com>
2024-11-25 12:40:17 +0200

    Update graphql.py

--- src/graphql.py
+++ src/graphql.py
@@ -6,6 +6,176 @@ import json
 
 logging.basicConfig(level=logging.DEBUG)  # Ensure logging is set up
 
+
+def get_repo_issues(owner, repository, duedate_field_name, after=None, issues=None):
+    query = """
+    query GetRepoIssues($owner: String!, $repo: String!, $duedate: String!, $after: String) {
+          repository(owner: $owner, name: $repo) {
+            issues(first: 100, after: $after, states: [OPEN]) {
+              nodes {
+                id
+                title
+                number
+                url
+                assignees(first:100) {
+                  nodes {
+                    name
+                    email
+                    login
+                  }
+                }
+                projectItems(first: 10) {
+                  nodes {
+                    project {
+                      number
+                      title
+                    }
+                    fieldValueByName(name: $duedate) {
+                      ... on ProjectV2ItemFieldDateValue {
+                        id
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
+                hasPreviousPage
+              }
+              totalCount
+            }
+          }
+        }
+    """
+
+    variables = {
+        'owner': owner,
+        'repo': repository,
+        'duedate': duedate_field_name,
+        'after': after
+    }
+
+    response = requests.post(
+        config.api_endpoint,
+        json={"query": query, "variables": variables},
+        headers={"Authorization": f"Bearer {config.gh_token}"}
+    )
+
+    if response.json().get('errors'):
+        print(response.json().get('errors'))
+
+    pageinfo = response.json().get('data').get('repository').get('issues').get('pageInfo')
+    if issues is None:
+        issues = []
+    issues = issues + response.json().get('data').get('repository').get('issues').get('nodes')
+    if pageinfo.get('hasNextPage'):
+        return get_repo_issues(
+            owner=owner,
+            repository=repository,
+            after=pageinfo.get('endCursor'),
+            issues=issues,
+            duedate_field_name=duedate_field_name
+        )
+
+    return issues
+
+
+
+def get_project_issues(owner, owner_type, project_number, duedate_field_name, filters=None, after=None, issues=None):
+    query = f"""
+    query GetProjectIssues($owner: String!, $projectNumber: Int!, $duedate: String!, $after: String)  {{
+          {owner_type}(login: $owner) {{
+            projectV2(number: $projectNumber) {{
+              id
+              title
+              number
+              items(first: 100, after: $after) {{
+                nodes {{
+                  id
+                  fieldValueByName(name: $duedate) {{
+                    ... on ProjectV2ItemFieldDateValue {{
+                      id
+                      date
+                    }}
+                  }}
+                  content {{
+                    ... on Issue {{
+                      id
+                      title
+                      number
+                      state
+                      url
+                      assignees(first: 20) {{
+                        nodes {{
+                          name
+                          email
+                          login
+                        }}
+                      }}
+                    }}
+                  }}
+                }}
+                pageInfo {{
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                }}
+                totalCount
+              }}
+            }}
+          }}
+        }}
+    """
+
+    variables = {
+        'owner': owner,
+        'projectNumber': project_number,
+        'duedate': duedate_field_name,
+        'after': after
+    }
+
+    response = requests.post(
+        config.api_endpoint,
+        json={"query": query, "variables": variables},
+        headers={"Authorization": f"Bearer {config.gh_token}"}
+    )
+
+    if response.json().get('errors'):
+        logging.error(response.json().get('errors'))
+        return []
+
+    page_info = response.json().get('data').get(owner_type).get('projectV2').get('items').get('pageInfo')
+    nodes = response.json().get('data').get(owner_type).get('projectV2').get('items').get('nodes')
+
+    if filters:
+        filtered_issues = []
+        for node in nodes:
+            if filters.get('open_only') and node['content'].get('state') != 'OPEN':
+                continue
+            filtered_issues.append(node)
+        nodes = filtered_issues
+
+    issues = issues or []
+    issues += nodes
+
+    if page_info.get('hasNextPage'):
+        return get_project_issues(
+            owner=owner,
+            owner_type=owner_type,
+            project_number=project_number,
+            after=page_info.get('endCursor'),
+            filters=filters,
+            issues=issues,
+            duedate_field_name=duedate_field_name
+        )
+
+    return issues
+
+
+
+
 # Fetch Project ID by Title
 def get_project_id_by_title(owner, project_title):
     query = """
