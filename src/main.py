0f7f00d693ba7a8dc84015ea9b57f4800d6405e1
Emily <emily-l93@hotmail.com>
2024-11-27 11:48:52 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -95,23 +95,20 @@ def release_based_on_duedate():
                         item_id = item['id']
                         item_found = True
 
-
-                        if due_date_obj == '2024-12-26':
-                        
-                            # Update the issue with the corresponding release option ID
-                            updated = graphql.update_issue_release(
-                                owner=config.repository_owner,
-                                project_title=project_title,
-                                project_id=project_id,
-                                release_field_id=release_field_name,
-                                item_id=item_id,
-                                release_option_id='8a45317c'
-                            )
-                            if updated:
-                                logging.info(f"Successfully updated issue {issue_id} to release {matching_release_name}.")
-                            else:
-                                logging.error(f"Failed to update issue {issue_id}.")
-                            break  # Exit the loop once the issue is updated
+                        # Update the issue with the corresponding release option ID
+                        updated = graphql.update_issue_release(
+                            owner=config.repository_owner,
+                            project_title=project_title,
+                            project_id=project_id,
+                            release_field_id=release_field_name,
+                            item_id=item_id,
+                            release_option_id=release_option_id
+                        )
+                        if updated:
+                            logging.info(f"Successfully updated issue {issue_id} to release {matching_release_name}.")
+                        else:
+                            logging.error(f"Failed to update issue {issue_id}.")
+                        break  # Exit the loop once the issue is updated
 
         except ValueError as e:
             logging.error(f"Error parsing due date for issue {project_item.get('title')}: {e}")
