4059c7c83841b7643ee6e8011a9f051a7e67e5b8
Emily <emily-l93@hotmail.com>
2024-12-05 15:20:14 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -13,7 +13,12 @@ project_number = int(os.environ['INPUT_PROJECT_NUMBER'])
 project_title = os.environ['INPUT_PROJECT_TITLE']
 api_endpoint = os.environ.get('GITHUB_GRAPHQL_URL', 'https://github.intranet.unicaf.org/api/graphql')
 
-duedate_field_name = os.environ['INPUT_DUEDATE_FIELD_NAME']
-release_field_name = os.environ['INPUT_RELEASE_FIELD_NAME']
 status_field_name = os.environ['INPUT_STATUS_FIELD_NAME']
+duedate_field_name = os.environ['INPUT_DUEDATE_FIELD_NAME']
+timespent_field_name = os.environ['INPUT_TIMESPENT_FIELD_NAME']
+release_field_name = os.environ['INPUT_RELEASE_FIELD_NAME']
+estimate_field_name = os.environ['INPUT_ESTIMATE_FIELD_NAME']
+priority_field_name = os.environ['INPUT_PRIORITY_FIELD_NAME']
+size_field_name = os.environ['INPUT_SIZE_FIELD_NAME']
+week_field_name = os.environ['INPUT_WEEK_FIELD_NAME']
 
