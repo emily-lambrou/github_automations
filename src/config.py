8fabcbb9645e7e988e72d94cfe674f01f9c716c8
Emily <emily-l93@hotmail.com>
2025-01-25 11:41:03 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,23 +2,14 @@ import os
 
 repository_owner = os.environ['GITHUB_REPOSITORY_OWNER']
 repository_owner_type = os.environ['INPUT_REPOSITORY_OWNER_TYPE']
-repository = os.environ['GITHUB_REPOSITORY']
-repository_name = repository.split('/')[1]
+
 server_url = os.environ['GITHUB_SERVER_URL']
 is_enterprise = True if os.environ.get('INPUT_ENTERPRISE_GITHUB') == 'True' else False
 dry_run = True if os.environ.get('INPUT_DRY_RUN') == 'True' else False
 
 gh_token = os.environ['INPUT_GH_TOKEN']
 project_number = int(os.environ['INPUT_PROJECT_NUMBER'])
-project_title = os.environ['INPUT_PROJECT_TITLE']
-api_endpoint = os.environ.get('GITHUB_GRAPHQL_URL', 'https://github.intranet.unicaf.org/api/graphql')
+api_endpoint = os.environ['GITHUB_GRAPHQL_URL']
 
-status_field_name = os.environ['INPUT_STATUS_FIELD_NAME']
-duedate_field_name = os.environ['INPUT_DUEDATE_FIELD_NAME']
-timespent_field_name = os.environ['INPUT_TIMESPENT_FIELD_NAME']
-release_field_name = os.environ['INPUT_RELEASE_FIELD_NAME']
-estimate_field_name = os.environ['INPUT_ESTIMATE_FIELD_NAME']
-priority_field_name = os.environ['INPUT_PRIORITY_FIELD_NAME']
-size_field_name = os.environ['INPUT_SIZE_FIELD_NAME']
-week_field_name = os.environ['INPUT_WEEK_FIELD_NAME']
-
+comments_issue_number = 0 if os.environ.get('INPUT_COMMENTS_ISSUE_NUMBER') == 'False' else int(os.environ.get('INPUT_COMMENTS_ISSUE_NUMBER'))
+comments_issue_repo = False if os.environ.get('INPUT_COMMENTS_ISSUE_REPO') == 'False' else os.environ.get('INPUT_COMMENTS_ISSUE_REPO')
