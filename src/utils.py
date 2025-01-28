5612187f409a7955023ca8a98a3865f1424c2fc8
Emily <emily-l93@hotmail.com>
2025-01-28 10:59:06 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,34 @@
+import graphql
+import config
 from datetime import datetime, timedelta
 from logger import logger
+
+
+def prepare_duedate_comment(issue: dict, assignees: dict, due_date):
+    """
+    Prepare the comment from the given arguments and return it
+    """
+
+    comment = ''
+    if assignees:
+        for assignee in assignees:
+            comment += f'@{assignee["login"]} '
+    else:
+        logger.info(f'No assignees found for issue #{issue["number"]}')
+
+    comment += f'The Due Date is updated to: {due_date.strftime("%b %d, %Y")}.'
+    logger.info(f'Issue {issue["title"]} | {comment}')
+
+    return comment
+
+
+def check_comment_exists(issueId, expected_comment):
+    """Check if the comment already exists on the issue."""
+    comments = graphql.get_issue_comments(issueId)
+    for comment in comments:
+        if expected_comment in comment.get('body', ''):
+            return True
+    return False
 
 
 def find_week(weeks, date_str):
